267f83c706217a77dfe250d8f95f4bdac99cf425
kevin santana <kevin_santana120@hotmail.com>
2022-11-11 20:43:27 -0500

    creado proyecto en vs

--- Juegoatmega/src/main.cpp
+++ Juegoatmega/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <stdlib.h>
 #define LIMIT_Y=8
 #define LIMIT_X=8
 uint8_t PORT[8]={1,2,4,8,16,32,64};
@@ -14,6 +15,18 @@ typedef struct{
 jugador persona ;
 persona.pos_x=0;
 persona.pos_y=0;
+
+typedef struct{
+  int *pos_x;
+  int *pos_y;
+}bombas;
+
+bombas  bomba[15];
+for (int i=0;i<15;i++){
+  bomba[i].pos_x=rand() % (LIMIT_X-1);
+  bomba[i].pos_y=rand() % (LIMIT_Y-1);
+}
+
 
 
 return 0;
