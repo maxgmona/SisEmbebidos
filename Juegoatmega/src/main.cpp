860646e1ac258c500558b131d3c66a84523b0912
kevin santana <kevin_santana120@hotmail.com>
2022-11-11 20:32:25 -0500

    creado proyecto en vs

--- Juegoatmega/src/main.cpp
+++ Juegoatmega/src/main.cpp
@@ -1,6 +1,20 @@
 #include <Arduino.h>
+#define LIMIT_Y=8
+#define LIMIT_X=8
+uint8_t PORT[8]={1,2,4,8,16,32,64};
 
 int main(){
+ DDRB=0xff;
+ DDRD=0xff;
+
+typedef struct{
+  int *pos_x;
+  int *pos_y;
+}jugador;
+jugador persona ;
+persona.pos_x=0;
+persona.pos_y=0;
+
 
 return 0;
 }
